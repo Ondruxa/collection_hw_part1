c1b8e27e62187af2497e3eccdef0844a3641761f
Andrei Gorshkov <andruhinsans@mail.ru>
2024-10-08 19:26:09 +0300

    start

---
+++ src/main/java/com/collection/collectoin_homework/CollectoinHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.collection.collectoin_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectoinHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectoinHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collection/collectoin_homework/CollectoinHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collection.collectoin_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectoinHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
